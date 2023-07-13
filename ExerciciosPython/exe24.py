70b390424e2c44d7b540d058dcf86eab9af188a1
francisco salles gonçalves neto <francisco.salles87@gmail.com>
2023-07-13 15:36:34 -0400

    Create exe24.py

---
+++ ExerciciosPython/exe24.py
@@ -0,0 +1,8 @@
+num = int(input('Me diga um numero qualquer:'))
+resultado = num % 2
+
+if resultado == 0:
+    print('O número {} é Par'.format(num))
+else:
+    print('O número {} é Ímpar'.format(num))
+    
