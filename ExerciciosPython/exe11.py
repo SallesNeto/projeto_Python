9c970532348af167e9ab4a799f86344f87f8983f
francisco salles gonçalves neto <francisco.salles87@gmail.com>
2023-07-13 14:26:25 -0400

    Create exe11.py
    tocar Mp3

---
+++ ExerciciosPython/exe11.py
@@ -0,0 +1,5 @@
+import pygame
+pygame.init()
+pygame.mixer.music.load('aqui eu colo a musica com o nome correto, e tem que estar salva neste programa')
+pygame.mixer.music.play()
+pygame.event.wait()
