a747122fcd64cd6ca9c1d93c6ca3dbcbf8c767f7
francisco salles gonçalves neto <francisco.salles87@gmail.com>
2023-07-13 15:54:18 -0400

    Create exe29.py

---
+++ ExerciciosPython/exe29.py
@@ -0,0 +1,6 @@
+sal = float(input('Qual o salario do funcionário? R$'))
+if sal >= 1250.00:
+    aumento = (sal * 10 / 100) + sal
+else:
+      aumento = (sal * 15 / 100) + sal
+print('Quem ganhava R${:.2f} passa a ganhar R${:.2f} agora.'.format(sal, aumento))
