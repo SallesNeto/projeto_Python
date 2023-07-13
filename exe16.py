82819cfdba188b8646b1c4288dc30bcfeb1a84c5
francisco salles gonçalves neto <francisco.salles87@gmail.com>
2023-07-13 15:13:45 -0400

    Create exe16.py

---
+++ exe16.py
@@ -0,0 +1,8 @@
+n=int(input('digite um numero:'))
+ant= n-1
+suc= n+1
+dob= n*2
+tri= n*3
+raiz =n**(1/2)
+print('O antecessor é {}, o sucessor e {}'.format(ant,suc))
+print('O seu dobro é {}, seu triplo {}, sua raiz {:.2f}'.format(dob,tri, raiz))
