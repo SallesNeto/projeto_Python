78ae543c8c2e22f107467c6fe4636bf71ae58d82
francisco salles gonçalves neto <francisco.salles87@gmail.com>
2023-07-13 16:00:20 -0400

    Create exe31.py

---
+++ ExerciciosPython/exe31.py
@@ -0,0 +1,12 @@
+casa = float(input('Valor da casa: R$'))
+salario = float(input('Salario do comprador: R$'))
+anos = int(input('Quantos anos de financiamento ?'))
+prestação = casa /(anos * 12)
+minimo = salario * 30 / 100
+print('Para pagar uma casa de R$ {:.2f} em {} anos'.format(casa, anos),end=' ')
+print('a prestação será de R${:.2f} '.format(prestação))
+if prestação <= minimo:
+    print('Emprestimo concedido')
+else:
+    print('Empretimo Negado!')
+    
